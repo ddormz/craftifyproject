bc7a0ead5edced10b59047694aef7c3829d208b0
Daniel Orellana <56647956+ddormz@users.noreply.github.com>
2023-10-12 13:43:06 -0300

    Update settings.py

--- craftify/settings.py
+++ craftify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=^-_z7r9a@0%zu*)cl_emxtcr4p5(va(3@k^iiytcfq87q&3#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
